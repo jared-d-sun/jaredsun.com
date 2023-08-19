8b89164828a160f849369572d6ee2b33d4fe2a7b
Jared Sun <jared@jaredsun.com>
2023-08-18 19:06:40 -0700

    tweak home page appearance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
 export default function Home() {
   return (
-    <main>
-      <h1>Jared Sun</h1>
-      <h2>Engineer, Developer</h2>
-      <p>
-        This website is still in early development. Check back later for more
-        content.
-      </p>
+    <main className={inter.className}>
+      <div className="max-w-7xl mx-auto px-6 lg:px-8 pt-8 text-center">
+        <h1 className="text-4xl/loose text-center">Jared Sun</h1>
+        <h2 className="text-xl/relaxed">Engineer, Developer</h2>
+        <p className="mt-10">
+          This website is still in early development. Check back later for more
+          content.
+        </p>
+      </div>
     </main>
   );
 }
